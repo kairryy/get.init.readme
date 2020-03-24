4ace0aca28d755c2409d66585d40c3e85f5880e0
Kalys lasoma <kairy.@eblasomas-MacBook-Pro.local>
2020-03-24 17:53:37 +0700

    Adding berapaumur.html

---
+++ latihan2.js
@@ -0,0 +1,4 @@
+var umur = prompt("berapa umur mu ?")
+var hasil = umur*365.5
+console.log(hasil)
+alert( umur + " Years is roughly " + hasil + " days")
---
+++ latihan3.js
@@ -0,0 +1,7 @@
+var answer = prompt('are you there yet ?');
+while(answer !== 'yes' && answer !== 'yeah'){
+    var answer = prompt('are you there yet ?');
+
+}
+    alert('Hallo im Kalys khairy lasoma, my hobbies right now is cooding')
+
